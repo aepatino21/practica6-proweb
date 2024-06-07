147d3ae075f4601ac9f39dcbdc4bb5f19d64feb9
aepatino21 <aepatino.21@est.ucab.edu.ve>
2024-06-07 08:16:33 -0700

    feat: tasks are saved and loaded onload

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 // Selección de elementos
 const taskInput = document.getElementById("text-input");
 const taskContainer = document.getElementById("task-container");
+window.onload = loadData();
 
 // Función para crear un container de tarea
 function createTaskContainer() {
@@ -30,16 +31,35 @@ function addTask() {
         label.innerHTML = taskInput.value;
         label.setAttribute("for", checkbox.className);
 
+        // Función para tachar tareas
         checkbox.onclick = function(){
             if (checkbox.checked) {
                 label.style.textDecoration = "line-through";
             } else {
                 label.style.textDecoration = "none";
             }
+            saveData();
         }
 
         task.append(checkbox, label, editIcon, deleteIcon);
         taskContainer.append(task);
         taskInput.value = '';
     }
+    saveData();
+}
+
+// Función para eliminar una tarea
+
+
+// Función para eliminar toda la lista de tareas
+
+
+// Función para almacenar tareas en memoria
+function saveData() {
+    localStorage.setItem("data", taskContainer.innerHTML);
+}
+
+// Funcion para cargar tareas encontradas en memoria
+function loadData() {
+    taskContainer.innerHTML = localStorage.getItem("data");
 }
